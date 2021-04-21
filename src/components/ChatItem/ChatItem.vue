f3ea8ed7c6fd6396b33ee7a9be6cf5c9160a1f72
sukanta <sukantsh11@gmail.com>
2021-04-21 23:56:01 +0530

    chat ui

--- src/components/ChatItem/ChatItem.vue
+++ src/components/ChatItem/ChatItem.vue
@@ -2,7 +2,7 @@
     <div class="mt-2  mb-1 flex justify-end w-full" v-if="position == 'me'" >
         <div >
             <span class="block text-sm font-bold text-right mr-2"  >  Me </span>
-            <span class="block text-sm chat-text shadow-xl"> {{chat.message}} </span>
+            <span class="block text-sm chat-text shadow-xl text-right"> {{chat.message}} </span>
         </div>
     </div>
     <div  class="mt-2 mb-1" v-if="position == 'other'">
